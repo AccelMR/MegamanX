e22f36748aacda8c075fec13df6c4a7760d2d070
charre2017idv <idv17c.rcharreton@uartesdigitales.edu.mx>
2019-06-20 15:09:37 -0500

    Spiky Script Enemy

---
+++ Assets/Scripts/Entities/Enemies/Spiky.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiky : MonoBehaviour
+{
+    public Rigidbody2D m_spiky;
+    // Start is called before the first frame update
+    void Start()
+    {
+      m_spiky = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position += new Vector3(1, 0, 0);
+    }
+}
