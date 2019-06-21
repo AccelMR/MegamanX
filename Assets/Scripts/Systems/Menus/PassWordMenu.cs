6f5ad64c81538cb577dfb49d94e35939cfcefc36
Mick76 <idv18b.jcerda@uartesdigitales.edu.mx>
2019-06-21 14:53:37 -0500

    Pantalla de opciones y de passwords anadido

---
+++ Assets/Scripts/Systems/Menus/PassWordMenu.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class PassWordMenu : MonoBehaviour
+{
+    public SceneFlow sceneFlow;
+    public Image Puntero;
+    public Image Fade;
+    public Image Flecha;
+
+    public Animator[] Monitos = new Animator[12];
+    public Image[] NumBlocks = new Image[12];
+    int[] NumBlockID = { 0,0,0,0,0,0,0,0,0,0,0,0};
+    Vector2[] Positions = new Vector2[12];
+
+    public Sprite[] Numbers = new Sprite[8];
+
+    int menuIndex = 0;
+    int columna = 0;
+    int fila = 0;
+
+    bool canRandomActivate = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Positions[0] = new Vector2(-72.2f, 72.2f);
+        Positions[1] = new Vector2(-24.0f, 72.2f);
+        Positions[2] = new Vector2(24.0f, 72.2f);
+        Positions[3] = new Vector2(72.2f, 72.2f);
+
+        Positions[4] = new Vector2(-72.2f, 24);
+        Positions[5] = new Vector2(-24, 24);
+        Positions[6] = new Vector2(24, 24);
+        Positions[7] = new Vector2(72.0f, 24);
+
+        Positions[8] = new Vector2(-72.0f, -24);
+        Positions[9] = new Vector2(-24, -24);
+        Positions[10] = new Vector2(24, -24);
+        Positions[11] = new Vector2(72.2f, -24);
+
+        Monitos[0].SetBool("selected", true);
+        Monitos[0].SetBool("walk", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Menus();
+        if (canRandomActivate)
+        {
+            canRandomActivate = false;
+            StartCoroutine(ActivteRandomMonito());
+        }
+        Debug.Log(menuIndex);
+    }
+
+    void Menus()
+    {
+        if (!Flecha.enabled)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow)) //Aqui reemplazar por el nuevo input
+            {
+                fila++;
+                DeativateMonito();
+                menuIndex = columna + (fila * 4);
+                if (menuIndex >= 12)
+                {
+                    //ir a end
+                    ActivarFlecha();
+                    fila--;
+                    menuIndex = columna + (fila * 4);
+                }
+                else
+                {
+                    Puntero.transform.localPosition = Positions[menuIndex];
+                    ActivateMonito();
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.UpArrow)) //Aqui reemplazar por el nuevo input
+            {
+                fila--;
+                DeativateMonito();
+                menuIndex = columna + (fila * 4);
+
+                if (menuIndex <= -1)
+                {
+                    //ir a end
+                    ActivarFlecha();
+                    fila++;
+                    menuIndex = columna + (fila * 4);
+                }
+                else
+                {
+                    Puntero.transform.localPosition = Positions[menuIndex];
+                    ActivateMonito();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) //Aqui reemplazar por el nuevo input
+            {
+                columna--;
+                DeativateMonito();
+                menuIndex--;
+                if (columna == -1)
+                {
+                    columna = 3;
+                    menuIndex = columna + (fila * 4);
+                }
+                Puntero.transform.localPosition = Positions[menuIndex];
+                ActivateMonito();
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow)) //Aqui reemplazar por el nuevo input
+            {
+                columna++;
+                DeativateMonito();
+                menuIndex++;
+                if (columna == 4)
+                {
+                    columna = 0;
+                    menuIndex = columna + (fila * 4);
+                }
+                Puntero.transform.localPosition = Positions[menuIndex];
+                ActivateMonito();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                NumBlockID[menuIndex]++;
+                if (NumBlockID[menuIndex] >= 8)
+                {
+                    NumBlockID[menuIndex] = 0;
+                }
+                NumBlocks[menuIndex].sprite = Numbers[NumBlockID[menuIndex]];
+                NumBlocks[menuIndex].SetNativeSize();
+            }
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                NumBlockID[menuIndex]--;
+                if (NumBlockID[menuIndex] <= -1)
+                {
+                    NumBlockID[menuIndex] = 7;
+                }
+                NumBlocks[menuIndex].sprite = Numbers[NumBlockID[menuIndex]];
+                NumBlocks[menuIndex].SetNativeSize();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Return))//aqui poner el input de boton de start
+            {
+                StartCoroutine(sceneFlow.ChangeScene("MenuScene"));
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Flecha.enabled = false;
+                Puntero.enabled = true;
+                fila = 2;
+                menuIndex = columna + (fila * 4);
+                Puntero.transform.localPosition = Positions[menuIndex];
+                ActivateMonito();
+            }
+            else if(Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Flecha.enabled = false;
+                Puntero.enabled = true;
+                fila = 0;
+                menuIndex = columna + (fila * 4);
+                Puntero.transform.localPosition = Positions[menuIndex];
+                ActivateMonito();
+            }
+        }
+    }
+
+    IEnumerator ActivteRandomMonito()//para los monitos que se asoman de manera random
+    {
+        int randomMonito = Random.Range(0, 11);
+        if (randomMonito != menuIndex)
+        {
+            Monitos[randomMonito].SetBool("randAct", true);
+        }
+        yield return new WaitForSeconds(1.0f);
+        Monitos[randomMonito].SetBool("randAct", false);
+        canRandomActivate = true;
+    }
+
+    void ActivateMonito()
+    {
+        Monitos[menuIndex].SetBool("selected", true);
+        Monitos[menuIndex].SetBool("walk", true);
+    }
+
+    void DeativateMonito()
+    {
+        Monitos[menuIndex].SetBool("selected", false);
+        Monitos[menuIndex].SetBool("walk", false);
+    }
+
+    void ActivarFlecha()
+    {
+        Flecha.enabled = true;
+        Puntero.enabled = false;
+    }
+}
--- Assets/Scripts/Systems/SceneFlow.cs
+++ Assets/Scripts/Systems/SceneFlow.cs
@@ -7,23 +7,15 @@ using UnityEngine.Video;
 
 public class SceneFlow : MonoBehaviour
 {
-    public Image MegamanPuntero;
-    public Image Blast;
-    public Image FadeOut;
-    public Font fontBlue;
-    public Font fontOrange;
-    public Text[] menuTexts = new Text[3];
-    int menuIndex = 0;
+    public Image Fade;
+
     string currentSceneName;
 
-    Vector2[] positions = new Vector2[3];
 
     // Start is called before the first frame update
     void Start()
     {
-        positions[0] = new Vector2(-93, -59);
-        positions[1] = new Vector2(-93, -77);
-        positions[2] = new Vector2(-93, -95);
+       
     }
 
     private void Awake()
@@ -36,13 +28,7 @@ public class SceneFlow : MonoBehaviour
     {
         if (currentSceneName == "MenuScene")//si est en la escena del menu
         {
-            Menu();
 
-            if (Blast.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("blast") && Blast.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
-            {
-                rechargeBlast();
-                rechargeAnim();
-            }
         }
         if (currentSceneName == "Intro1")
         {
@@ -60,79 +46,11 @@ public class SceneFlow : MonoBehaviour
         }
     }
 
-    void Menu()
+    public IEnumerator ChangeScene(string sceneName, float delay = 0)
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow)) //Aqui reemplazar por el nuevo input
-        {
-            menuTexts[menuIndex].font = fontBlue;
-            menuIndex++;
-            if (menuIndex == 3)
-            {
-                menuIndex = 0;
-            }
-            else if (menuIndex == -1)
-            {
-                menuIndex = 2;
-            }
-            MegamanPuntero.transform.localPosition = positions[menuIndex];
-            menuTexts[menuIndex].font = fontOrange;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow)) //Aqui reemplazar por el nuevo input
-        {
-            menuTexts[menuIndex].font = fontBlue;
-            menuIndex--;
-            if (menuIndex == 3)
-            {
-                menuIndex = 0;
-            }
-            else if (menuIndex == -1)
-            {
-                menuIndex = 2;
-            }
-            menuTexts[menuIndex].font = fontOrange;
-            MegamanPuntero.transform.localPosition = positions[menuIndex];
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))//aqui poner el input de boton de start
-        {
-            MegamanPuntero .GetComponent<Animator>().SetBool("start", true);
-            Blast.GetComponent<Animator>().SetBool("start", true);
-            Blast.enabled = true;
-            switch (menuIndex)
-            {
-                case 0://start
-                    {
-                        StartCoroutine(ChangeScene("MainGame", 0.4f));
-                        break;
-                    }
-                case 1://password codes
-                    {
-                        break;
-                    }
-                case 2://options
-                    {
-                        break;
-                    }
-            }
-        }
-    }
-
-    IEnumerator ChangeScene(string sceneName, float delay = 0)
-    {
-        yield return new WaitForSeconds(delay);//esperar a que el blast salga de la pantalla
-        FadeOut.GetComponent<Animator>().enabled = true;//animar fade out
+        yield return new WaitForSeconds(delay);//si es necesario un delay
+        Fade.GetComponent<Animator>().enabled = true;//animar fade out
         yield return new WaitForSeconds(1.0f);//esperar a la animacion de fadeOut
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);//cargar escena
-    }
-
-    void rechargeBlast()
-    {
-        Blast.GetComponent<Animator>().SetBool("start", false);
-        Blast.enabled = false;
-    }
-
-    void rechargeAnim()
-    {
-        MegamanPuntero.GetComponent<Animator>().SetBool("start", false);
     }
 }
