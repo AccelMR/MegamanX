f88647c49c597a4c9d4cc306b6722802f0971379
Santiago Toll <idv16a.stoll@uartesdigitales.edu.mx>
2019-06-22 13:30:58 -0500

    WOrking on BeeBoss

--- Assets/Scripts/Camara/CameraController.cs
+++ Assets/Scripts/Camara/CameraController.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
   [SerializeField] float offset;
+  [SerializeField] float range;
   [SerializeField] List<Sprite> broken;
   Vector2 offsetVector;
+  bool isEnabled;
   // Start is called before the first frame update
   void Start()
   {
+    isEnabled = false;
     offsetVector = new Vector2(0.01258147f, 0.01558709f);
     transform.GetChild(1).GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+    gameObject.GetComponents<BoxCollider2D>()[1].enabled = false;
 
   }
 
   // Update is called once per frame
   void Update()
   {
+    if (isEnabled)
+    {
+    }
+    else
+    {
 
+    }
   }
   private void OnTriggerEnter2D(Collider2D collision)
   {
@@ -28,6 +38,8 @@ public class CameraController : MonoBehaviour
       camera.transform.parent = null;
       Vector3 desired = new Vector3(transform.position.x + offset, camera.transform.position.y, camera.transform.position.z);
       camera.transform.position = desired;
+      gameObject.GetComponents<BoxCollider2D>()[1].enabled = true;
+      isEnabled = true;
     }
     if (collision.tag == "Bullet")
     {
@@ -35,6 +47,7 @@ public class CameraController : MonoBehaviour
       transform.GetChild(1).GetComponent<Collider2D>().offset = offsetVector;
       transform.GetChild(1).GetComponent<Rigidbody2D>().gravityScale = 2;
       transform.GetChild(1).GetComponent<Rigidbody2D>().constraints = ~RigidbodyConstraints2D.FreezePositionY;
+      gameObject.GetComponents<BoxCollider2D>()[1].enabled = false;
 
       GameObject.FindGameObjectWithTag("Player").GetComponent<Megaman>().setAnim(ANIM_STATE.FALL);
       GameObject.FindGameObjectWithTag("Player").transform.parent = GameObject.Find("Middle").transform;
